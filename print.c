bb0033c45ea53a744f06105c5ada0d8de57abe86
MD Mozammel Hoq <mozammelhoq512@gmail.com>
2023-04-06 03:33:03 +0600

    variable in c

---
+++ print.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main ()
+{
+    printf("Hello World!\nNew Line\tNew world\n");
+    printf("Now i am a programmer \n");
+    printf("print back slash : \\ \n");
+    printf("print percentage : %% \n");
+    return 0;
+}
+
+/* for new line : \n
+ for tab : \t
+ for back slash :  
+ for percentage : \%
+*/
---
+++ variable.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+int main ()
+{
+    int a = 10;
+    float b = 10.5;
+    char c = '@';
+    printf("a = %d\nb = %0.2f\nc = %c", a , b, c);
+    
+    return 0;
+}
+
+
+
+/*
+int : here we can store Integer value like -100, 0, 100
+float : here we can store decimal value like 0.0, 1.0, 1.5
+char : here we can store character like 'a', 'b', 'c', '@' etc
+bool : here we can store boolean value like true, false
+
+*/
